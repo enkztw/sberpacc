55c489eefd5c3f951b1e0ba9abeff68bdb301997
enkztw <jztenk@gmail.com>
2019-10-02 14:58:07 +0300

    2nd order func refactor

--- 3. JS 01.10.2019/order2.js
+++ 3. JS 01.10.2019/order2.js
@@ -26,33 +26,49 @@ const box = {
 
 const checkOrderItem = (item, menu) => menu.includes(item);
 
-const checkIngredients = (coffee, recipes, box) => {
-    for (const item of recipes[coffee]) {
-        if (box[item] === 0) {
-            console.log(`--Памятка: Нужно заказать ${item}`);
-            return false;
-        }
+// const checkIngredients = (coffee, recipes, box) => {
+//     if (!coffee) {
+//         return;
+//     }
+
+//     for (const item of recipes[coffee]) {
+//         if (box[item] === 0) {
+//             console.log(`--Памятка: Нужно заказать ${item}.`);
+//             return false;
+//         }
+//     }
+
+//     return true;
+// };
+
+
+// Аналогия ф-ции выше, наличие кофе в заказе теперь проверяется в контроллере order(),
+// Проверяем есть ли нехватка ингридиентов
+
+const checkIngredientsLack = (coffee, recipes, box) => recipes[coffee].some((item) => {
+    if (box[item] === 0) {
+    console.log(`--Памятка: Нужно заказать ${item}.`);
+
+        return true;
     }
+});
 
-    return true;
-};
-
-function order(coffee, cookie) {
+function order(coffee, cookie = null) {
     const dictionary = {
-        allOk: `Вот ваш заказ: ${coffee} и ${cookie} печенье! Хорошего дня!`,
+        allOk: `Вот ваш заказ: ${coffee ? coffee : ``}${coffee && cookie ? ` и `: ``}${cookie ? `${cookie} печенье` : ``}. Хорошего дня!`,
         allNotOk: `Мы не можем обработать ваш заказ, извините.`,
-        noCookie: `К сожалению, у нас нет такого печенья. Вот ваш ${coffee}`,
-        noCoffee: `К сожалению, сейчас не можем приготовить ${coffee}. Вот ваше ${cookie} печенье`,
+        noCookie: `К сожалению, у нас нет такого печенья. Вот ваш ${coffee}.`,
+        noCoffee: `К сожалению, сейчас не можем приготовить ${coffee}. Вот ваше ${cookie} печенье.`,
     }
 
     const isCookie = checkOrderItem(cookie, cookies);
 
     const isCoffee = checkOrderItem(coffee, Object.keys(recipes));
 
-    const isIngridients = checkIngredients(coffee, recipes, box);
+    const isIngridients = coffee ? !checkIngredientsLack(coffee, recipes, box) : `Кофе нет в заказе.`;
 
     switch (true) {
-        case isCoffee && isCookie && isIngridients:
+        case (coffee === null || (isCoffee && isIngridients)) && (cookie === null || isCookie):
             console.log(dictionary.allOk);
             break;
         case !isCookie && !isIngridients:
@@ -77,17 +93,23 @@ order('американо', 'мятное')
 order('баунти раф', 'апельсиновое')
 order('банановый латте', 'мятное')
 order('баунти раф', 'малиновое')
+order('американо')
+order(null, 'мятное')
 
 // Ожидаемый вывод //
 /*
 "Вот ваш заказ: американо и мятное печенье! Хорошего дня!"
 "-----Следующий заказ-----"
-"--Памятка: Нужно заказать кокосовое молоко"
+"--Памятка: Нужно заказать кокосовое молоко."
 "Мы не можем обработать ваш заказ, извините."
 "-----Следующий заказ-----"
 "Вот ваш заказ: банановый латте и мятное печенье! Хорошего дня!"
 "-----Следующий заказ-----"
-"--Памятка: Нужно заказать кокосовое молоко"
-"К сожалению, сейчас не можем приготовить баунти раф. Вот ваше малиновое печенье"
+"--Памятка: Нужно заказать кокосовое молоко."
+"К сожалению, сейчас не можем приготовить баунти раф. Вот ваше малиновое печенье."
+"-----Следующий заказ-----"
+""Вот ваш заказ: американо. Хорошего дня!"
+"-----Следующий заказ-----"
+"Вот ваш заказ: мятное печенье. Хорошего дня!"
 "-----Следующий заказ-----"
 */
