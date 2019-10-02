632f6003f88cd447c85c182b3210ee96f80acd94
enkztw <jztenk@gmail.com>
2019-10-02 16:58:06 +0300

    makeMeACoffee v2

--- 3. JS 01.10.2019/closures.js
+++ 3. JS 01.10.2019/closures.js
@@ -13,3 +13,26 @@ console.log(cocoblond);
 console.log(icedark);
 
 
+const makeMeACoffeeV2 = (order) => {
+    let pshik = 2;
+    const ingridients = order.join(`, `);
+
+    const makeACoffee = () => ingridients.concat(` кофе`);
+    const addSyrup = () => ingridients.concat(` кофе с ${++pshik} пшиками карамели`);
+
+    return {
+        addSyrup: () => addSyrup(),
+        noSyrup: () => makeACoffee()
+    }
+};
+
+const icedark2 = makeMeACoffeeV2(['лед', 'темная обжарка']);
+const cocoblond2 = makeMeACoffeeV2(['малиновый сироп', 'кокосовое молоко', 'светлая обжарка']);
+
+console.log(icedark2.noSyrup());
+console.log(cocoblond2.addSyrup());
+console.log(cocoblond2.addSyrup());
+console.log(cocoblond2.addSyrup());
+console.log(cocoblond2.addSyrup());
+console.log(cocoblond2.addSyrup());
+console.log(cocoblond2.addSyrup());
