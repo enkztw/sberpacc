353d3c913b71fbaa4be42f33dced6a13e720e5df
KuznetsovMA <jztenk@gmail.com>
2019-10-06 18:30:12 +0300

    disabling buttons

--- Coffee Machine/js/fill.js
+++ Coffee Machine/js/fill.js
@@ -1,12 +1,66 @@
 const cup = document.querySelector(`.machine__fill`);
 const water = document.getElementById(`water`);
-let percent = cup.getAttribute(`data-percent`);
+const drinksButtons = document.querySelectorAll(`.drink`);
+const machineCounter = document.querySelector(`.machine__counter`);
 
-const interval = setInterval(() => {
-    percent++;
-
-    water.style.transform = 'translate(0' + ',' + (100 - percent) + '%)';
-    if (percent === 100) {
-        clearInterval(interval);
+const CUP = {
+    'small': {
+        'amount': 5,
+        'ml': 250
+    },
+    'big': {
+        'amount': 6,
+        'ml': 380
     }
-}, 60);
+}
+
+const drinks = {
+    'espresso': {
+        'ml': 100
+    },
+    'latte': {
+        'ml': 250
+    },
+    'cappuccino': {
+        'ml': 250
+    }
+}
+
+let currentDrink = ``;
+let fillPercent = 0;
+let fillMl = 0;
+
+
+const disableDrinks = (items) => items.forEach((item) => item.classList.add(`drink--disabled`));
+const onDrinkButtonClick = (evt) => {
+    if (!currentDrink) {
+        const drinkButton = evt.target;
+        const drinkName = drinkButton.getAttribute(`data-name`);
+        const drinkAmount = drinks[drinkName].ml;
+
+        currentDrink = drinkName;
+        fillMl += drinkAmount;
+        fillPercent += 100 / CUP.big.ml * drinkAmount;
+
+        disableDrinks(drinksButtons);
+
+        drinkButton.classList.remove(`drink--disabled`);
+        drinkButton.classList.add(`drink--selected`);
+        
+        machineCounter.textContent = `${fillMl}ML`;
+        water.style.transform = `translate(0, ${100 - fillPercent}%)`;
+    }
+};
+
+drinksButtons.forEach((drinkButton) => drinkButton.addEventListener(`click`, onDrinkButtonClick));
+
+// const interval = setInterval(() => {
+//     percent++;
+
+//     water.style.transform = `translate(0, ${100 - percent}%)`;
+//     if (percent === 100) {
+//         clearInterval(interval);
+//     }
+
+//     console.log(percent);
+// }, 60);
