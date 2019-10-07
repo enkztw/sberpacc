f6ea7cee21d4b410b720843e08ddcc51ea66b81c
enkztw <jztenk@gmail.com>
2019-10-07 16:36:55 +0300

    drink creation

--- Coffee Machine/js/fill.js
+++ Coffee Machine/js/fill.js
@@ -1,13 +1,17 @@
 const water = document.getElementById(`water`);
+const cup = document.querySelector(`.machine__cup`);
+
+const price = document.querySelector(`.price .value`);
+const smallCups = document.querySelector(`.cup--small b`);
+const bigCups = document.querySelector(`.cup--big b`);
+const nameField = document.querySelector(`.name`);
 
 const drinkButtons = document.querySelectorAll(`.drink`)
 const defaultDrinks = document.querySelectorAll(`.drink--default`);
 const customDrinks = document.querySelectorAll(`.drink--custom`);
 
-
 const additionalButtons = document.querySelectorAll(`.additional`);
 const [milkButton, syrupButton] = additionalButtons;
-
 
 const payButton = document.querySelector(`.pay`);
 const resetButton = document.querySelector(`.reset`);
@@ -67,7 +71,8 @@ let currentDrink = {
     'additionals': {
         'milk': 0,
         'syrup': 0
-    }
+    },
+    'type': null
 };
 
 let currentPrice = 0;
@@ -90,16 +95,87 @@ const fillDrink = (drinkAmount, drinkPrice) => {
     }
 
     checkSpace();
+    setName();
+    setPrice();
 };
 
+
 const dumpDrink = () => {
-    currentDrink = ``;
+    currentDrink.base.name = null;
+    currentDrink.type = null;
+    currentDrink.base.portion = 0;
+    currentDrink.additionals.milk = 0;
+    currentDrink.additionals.syrup = 0;
+
     currentPrice = 0;
     fillPercent = 0;
     fillMl = 0;
 
     water.style.transform = `translate(0, 110%)`;
     machineCounter.textContent = `${fillMl}ML`;
+};
+
+
+const removeCup = () => {
+    cup.classList.add(`machine__cup--filled`);
+    if (fillMl <= CUP.small.ml) {
+        --CUP.small.amount;
+        smallCups.textContent = CUP.small.amount;
+    } else {
+        --CUP.big.amount;
+        bigCups.textContent = CUP.big.amount;
+    }
+};
+
+const onDrinkCreated = () => {
+    water.style.transitionDuration = `2s`;
+    cup.classList.add(`machine__cup--filled`);
+    removeCup();
+};
+
+const createDrink = () => {
+    const creationTimeMap = {
+        'default': 3,
+        'author': 5,
+        'custom': 8,
+    };
+
+    let currentFill = 0;
+    let currentPercent = 0;
+    
+    water.style.transitionDuration = `0s`;
+    water.style.transform = `translate(0, 110%)`;
+    machineCounter.textContent = `0ML`;
+
+
+    console.log(fillPercent);
+    console.log(fillMl);
+
+
+    const animateFrame = () => {
+        if (currentFill < fillMl) {
+            ++currentFill;
+            currentPercent += (fillPercent + 10) / fillMl;
+            water.style.transform = `translate(0, ${110 - currentPercent}%)`;
+            machineCounter.textContent = `${currentFill}ML`;
+        } else {
+            onDrinkCreated();
+            clearInterval(frameInterval);
+        }
+    };
+
+    const frameInterval = setInterval(animateFrame, creationTimeMap[currentDrink.type] * 1000 / fillMl);
+
+};
+
+const setPrice = () => price.textContent = currentPrice;
+const setName = () => {
+    const getAdditionalName = () => Object.values(currentDrink.additionals).reduce((milk, syrup) => `${milk ? `${milk}x MILK` : ``}${milk && syrup ? ` ` : ``}${syrup ? `${syrup}x SYRUP` : ``}`);
+
+    const baseName = `${currentDrink.base.name ? `${currentDrink.base.portion}x ${currentDrink.base.name.toUpperCase()}` : ``}`;
+    const additionalName = getAdditionalName();
+
+    nameField.textContent = `${baseName}${baseName && additionalName ? ` & ` : ``}${additionalName ? `${additionalName}` : ``}`;
 };
 
 const checkSpace = () => {
@@ -132,15 +208,16 @@ const onDrinkClick = (evt) => {
     currentDrink.base.name = drinkName;
     currentDrink.base.portion++;
 
-    console.log(currentDrink);
     drinkButton.classList.add(`selected`);
 
     disable([...defaultDrinks, ...customDrinks]);
     enable([payButton, resetButton, drinkButton]);
 
     if (drinkButton.classList.contains(`drink--custom`)) {
+        currentDrink.type = `author`;
         disable([milkButton]);
     } else {
+        currentDrink.type ? `` : currentDrink.type = 'default';
         enable([syrupButton]);
     }
 
@@ -153,9 +230,9 @@ const onAdditionalClick = (evt) => {
     const additionalAmount = drinks[additionalName].ml;
     const additionalPrice = drinks[additionalName].price;
 
+    currentDrink.type = `custom`;
     currentDrink.additionals[additionalName]++;
 
-    console.log(currentDrink);
     additionalButton.classList.add(`selected`);
 
     disable(customDrinks);
@@ -165,6 +242,7 @@ const onAdditionalClick = (evt) => {
 };
 
 const onResetClick = () => {
+    nameField.textContent = ``;
     enable([...drinkButtons, milkButton]);
     disable([payButton, resetButton, syrupButton]);
     unselect([...drinkButtons, milkButton, syrupButton]);
@@ -172,8 +250,18 @@ const onResetClick = () => {
     dumpDrink();
 };
 
+const onPayClick = () => {
+    // onResetClick();
+    disable([payButton, resetButton, syrupButton, milkButton, ...customDrinks, ...defaultDrinks]);
+
+    console.log(currentDrink);
+    createDrink();
+
+};
+
 drinkButtons.forEach((drinkButton) => drinkButton.addEventListener(`click`, onDrinkClick));
 additionalButtons.forEach((additionalButton) => additionalButton.addEventListener(`click`, onAdditionalClick));
 resetButton.addEventListener(`click`, onResetClick);
+payButton.addEventListener(`click`, onPayClick);
 
 checkSpace();
