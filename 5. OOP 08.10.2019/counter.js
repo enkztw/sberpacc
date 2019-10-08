aa7905925f42d915d7c9ad2df31f5409412e85d9
enkztw <jztenk@gmail.com>
2019-10-08 19:39:17 +0300

    counter

---
+++ 5. OOP 08.10.2019/counter.js
@@ -0,0 +1,26 @@
+const counter = {
+    count: 0,
+    add() {
+        this.count++;
+    }
+};
+
+
+const clock = {
+    ticktock() {
+        setInterval(() => {
+            this.add.call(counter);
+        }, 1000);
+    }
+};
+
+Object.setPrototypeOf(clock, counter);
+
+clock.ticktock();
+
+counter.count = 3600;
+
+setInterval(() => {
+    console.log(counter.count);
+}, 1000);
+
