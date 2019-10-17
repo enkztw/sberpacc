e402270512d65dc1fd4effc53ca094f429dd400a
enkztw <jztenk@gmail.com>
2019-10-17 19:35:31 +0300

    Tic tac toe game

--- 7. React 15.10.2019/my-app/src/App.js
+++ 7. React 15.10.2019/my-app/src/App.js
@@ -1,93 +1,67 @@
 import React from 'react';
 import './App.css';
 
-const MyButton = (props) => {
-  const style = {
-    backgroundColor: '#19bb4f',
-    color: '#fff',
-    padding: '20px',
-    fontSize: '1.5rem',
-    fontFamily: 'Roboto',
-    textTransform: 'uppercase'
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    }
   }
-  return <button style={style} onClick={props.handler} className='my-button'>{props.str}</button>
-}
-
-/**
- * Как высчитываются очки
- * 
- *.----------.------.-------.----------.
- *|          | Rock | Paper | Scissors |
- *:----------+------+-------+----------:
- *| Rock     |    0 |     0 |        1 |
- *:----------+------+-------+----------:
- *| Paper    |    1 |     0 |        0 |
- *:----------+------+-------+----------:
- *| Scissors |    0 |     1 |        0 |
- *'----------'------'-------'----------'
- * */
-
-const truthTable = [
-    [0,0,1],
-    [1,0,0],
-    [0,1,0]
-]
-
-const checkWinner = (playerChoice, computerChoice) => {
-    const playerScore = truthTable[playerChoice][computerChoice];
-    const computerScore = truthTable[computerChoice][playerChoice];
-    if (playerScore > computerScore) {
-        return 'Ты';
-    } else if (computerScore > playerScore) {
-        return 'Комп';
-    } else if (playerScore === computerScore) {
-        return 'Никто';
-    }
-}
-
-const buttons = [{str: 'камень', type: 'rock', score: 0}, {str: 'бумага', type: 'paper', score: 1}, {str: 'ножницы', type: 'scissors', score: 2}]
-
-export default class App extends React.Component {
-
-    state = {
-        compChoice: '',
-        userChoice: ''
-    }
-    
-    getRandomNum = (min, max) => {
-      min = Math.ceil(min)
-      max = Math.floor(max)
-      return Math.floor(Math.random() * (max - min + 1)) + min
-  }
-
-    onClickHandler = (score) => {
-      const compChoice = this.getRandomNum(0, 2);
-      const userChoice = score;
-      this.setState({
-        compChoice,
-        userChoice,
-      })
-    }
-
-
   render() {
-    const compChoiceName = (this.state.compChoice !== ``) && buttons[this.state.compChoice].str;
-    const userChoiceName = (this.state.userChoice !== ``) && buttons[this.state.userChoice].str;
-    const winner = (this.state.userChoice !== `` && this.state.compChoice !== ``) && checkWinner(this.state.userChoice, this.state.compChoice);
     return (
-        <div className="App">
-          <header className="App-header">
-              <p>Камень, Ножницы, Бумага!</p>
-          </header>
-            <div>
-                {buttons.map((button) => <MyButton key={button.score} {...button} handler={() => this.onClickHandler(button.score)} />)}
-            </div>
-            <div>
-            <p>Комп выбрал: {compChoiceName}</p>
-            <p>А твой выбор: {userChoiceName}</p>
-            <p>Победитель: {winner}</p>
-            </div>
-        </div>
+      <button onClick={() => this.setState({value: this.props.num})}className="square">
+        {this.state.value}
+      </button>
     );
   }
 }
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square num={i}/>;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
