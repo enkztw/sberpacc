a1e9c298c37ad93ff39527e33cbc7c8716c246aa
enkztw <jztenk@gmail.com>
2019-10-21 17:31:36 +0300

    Updates

---
+++ precache-manifest.41f2454bbcc1c2189b4ff68ff92e71bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012aebc4e96aa1d4013091931778e38f",
+    "url": "/sberbank-practice/Deposit%20Converter/app/index.html"
+  },
+  {
+    "revision": "2b0397c2bb542f76ff66",
+    "url": "/sberbank-practice/Deposit%20Converter/app/static/css/main.b2545349.chunk.css"
+  },
+  {
+    "revision": "3844f28d2ecc4e4e0424",
+    "url": "/sberbank-practice/Deposit%20Converter/app/static/js/2.271f4837.chunk.js"
+  },
+  {
+    "revision": "2b0397c2bb542f76ff66",
+    "url": "/sberbank-practice/Deposit%20Converter/app/static/js/main.d3933610.chunk.js"
+  },
+  {
+    "revision": "d5c49a94bee8a1b1ba23",
+    "url": "/sberbank-practice/Deposit%20Converter/app/static/js/runtime-main.c4be6029.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sberbank-practice/Deposit%20Converter/app/precache-manifest.41f2454bbcc1c2189b4ff68ff92e71bc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sberbank-practice/Deposit%20Converter/app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
