858ff708120fc70ac4e52f5d640a8ae9ad88abe9
enkztw <jztenk@gmail.com>
2019-10-22 21:29:59 +0300

    Local storage

--- 9. Redux 22.10.2019/redux/src/store/index.js
+++ 9. Redux 22.10.2019/redux/src/store/index.js
@@ -1,8 +1,12 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 
 import * as reducers from './reducers'
 import * as actions from './actions'
 
+const middleware = store => dispatch => action => {
+    dispatch(action)    
+    localStorage.setItem('App_store', JSON.stringify(store.getState()))
+}
 
 export { actions }
-export default createStore(combineReducers(reducers), {})
+export default createStore(combineReducers(reducers), JSON.parse(localStorage.getItem('App_store') || ''), applyMiddleware(middleware))
