a555eb99cf5a14403edd8f457171175565506d3c
enkztw <jztenk@gmail.com>
2019-10-23 13:05:05 +0300

    reducer init

--- 9. Redux 22.10.2019/redux/src/state.js
+++ 9. Redux 22.10.2019/redux/src/state.js
@@ -8,3 +8,14 @@
 
 // Flux standart action
 // {type: 'STR', payload: ...., error}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'INCREASE':
+            return Object.assign({}, state, { counter: state.counter + 1 })
+        case 'RESET':
+            return Object.assign({}, state, { counter: 0 })
+        default:
+            return state
+    }
+}
